3bd8d219d9a31ce7b2cb6dd1a86c85904de6f8d3
dieunor <99774254+dieunor@users.noreply.github.com>
2025-01-07 11:41:38 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetris-game" # Replace with your actual S3 bucket name
+    bucket = "tetrisgamev1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
